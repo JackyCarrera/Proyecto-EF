ca4b450c1cf1372635db8bee7fb4582e54e65024
JackyCarrera <jackycarrera1234@gmail.com>
2020-06-11 14:42:16 -0600

    Procesos

---
+++ Examen final- Jaqueline Gabriela Carrera Rosario-9959-19-12361/Proyecto EF/src/clases/imagenfondo.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Insets;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.border.Border;
+
+/**
+ *
+ * @author Jacky
+ */
+public class imagenfondo implements Border {
+public BufferedImage back;
+
+public imagenfondo(){
+    try {
+        URL imagePath = new URL(getClass().getResource("../Contenedor/ea094b51936760877fc5907845391bc9.jpg").toString());
+        back = ImageIO.read(imagePath);
+    }catch(Exception ex){
+        
+    }
+}
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        g.drawImage(back, (x + (width - back.getWidth())/2),(y + (height - back.getHeight())/2), null);
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+      return new Insets(0,0,0,0);  
+    }
+
+    @Override
+    public boolean isBorderOpaque() {
+    return false;
+    }
+}
+
